aa534d9d9c9cd6a31db4ad28961a51e8eac02f37
guillaume-delattre <guillaume.delattre@hotmail.fr>
2014-06-06 16:48:41 +0200

    Add converters-validators examples

---
+++ jsflab/converters-and-validators/src/main/java/org/gdelattre/jsf/ReceiptBean.java
@@ -0,0 +1,33 @@
+package org.gdelattre.jsf;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+ 
+@ManagedBean(name="receipt")
+@SessionScoped
+public class ReceiptBean implements Serializable{
+ 
+	double amount;
+	
+	Date date;
+	 
+	public double getAmount() {
+		return amount;
+	}
+ 
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+ 
+	public void setDate(Date date) {
+		this.date = date;
+	}
+ 
+}
